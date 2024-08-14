7c50651508d3ea3de1f23d0f2e49763f15137f16
hdc <nomad.hwang@gmail.com>
2024-08-14 12:01:06 +0000

    fix: siteMetadata URL

--- data/projectsData.ts
+++ data/projectsData.ts
@@ -7,20 +7,10 @@ interface Project {
 
 const projectsData: Project[] = [
   {
-    title: 'A Search Engine',
-    description: `What if you could look up any information in the world? Webpages, images, videos
-    and more. Google has many features to help you find exactly what you're looking
-    for.`,
+    title: 'Moadoom',
+    description: `Helps you manage your social media content. Publish to YouTube, TikTok, and Instagram all in one place.`,
     imgSrc: '/static/images/google.png',
-    href: 'https://www.google.com',
-  },
-  {
-    title: 'The Time Machine',
-    description: `Imagine being able to travel back in time or to the future. Simple turn the knob
-    to the desired date and press "Go". No more worrying about lost keys or
-    forgotten headphones with this simple yet affordable solution.`,
-    imgSrc: '/static/images/time-machine.jpg',
-    href: '/blog/the-time-machine',
+    href: 'https://moadoom.com',
   },
 ]
 
--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -2,12 +2,12 @@
 const siteMetadata = {
   title: 'TurtiesIO SaaS',
   author: 'TurtiesIO',
-  headerTitle: 'TailwindBlog',
-  description: 'A blog created with Next.js and Tailwind.css',
+  headerTitle: 'TurtiesIO',
+  description: "TurtiesIO's SaaS Entrepreneurship Blog",
   language: 'en-us',
   theme: 'system', // system, dark or light
-  siteUrl: 'https://tailwind-nextjs-starter-blog.vercel.app',
-  siteRepo: 'https://github.com/timlrx/tailwind-nextjs-starter-blog',
+  siteUrl: 'https://turties.io',
+  siteRepo: 'https://github.com/turtiesio/turtiesio-blog',
   siteLogo: `${process.env.BASE_PATH || ''}/static/images/logo.png`,
   socialBanner: `${process.env.BASE_PATH || ''}/static/images/twitter-card.png`,
   mastodon: 'https://mastodon.social/@mastodonuser',
@@ -58,6 +58,7 @@ const siteMetadata = {
     // Select a provider and use the environment variables associated to it
     // https://vercel.com/docs/environment-variables
     provider: 'giscus', // supported providers: giscus, utterances, disqus
+
     giscusConfig: {
       // Visit the link below, and follow the steps in the 'configuration' section
       // https://giscus.app/
@@ -67,17 +68,22 @@ const siteMetadata = {
       categoryId: process.env.NEXT_PUBLIC_GISCUS_CATEGORY_ID,
       mapping: 'pathname', // supported options: pathname, url, title
       reactions: '1', // Emoji reactions: 1 = enable / 0 = disable
+
       // Send discussion metadata periodically to the parent window: 1 = enable / 0 = disable
       metadata: '0',
+
       // theme example: light, dark, dark_dimmed, dark_high_contrast
       // transparent_dark, preferred_color_scheme, custom
       theme: 'light',
+
       // theme when dark mode
       darkTheme: 'transparent_dark',
+
       // If the theme option above is set to 'custom`
       // please provide a link below to your custom theme css file.
       // example: https://giscus.app/themes/custom_example.css
       themeURL: '',
+
       // This corresponds to the `data-lang="en"` in giscus's configurations
       lang: 'en',
     },
@@ -87,6 +93,7 @@ const siteMetadata = {
     kbarConfig: {
       searchDocumentsPath: `${process.env.BASE_PATH || ''}/search.json`, // path to load documents to search
     },
+
     // provider: 'algolia',
     // algoliaConfig: {
     //   // The application ID provided by Algolia
